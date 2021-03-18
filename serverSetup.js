5fc235b49ee109042540f2f9de89976097b91268
Arkan Abdel Qahar Zenal <arkan.zenal@crealogix.com>
2021-03-18 20:23:08 +0100

    more Pointer adding

--- serverSetup.js
+++ serverSetup.js
@@ -4,4 +4,6 @@ r.db("CollabDB").tableCreate('points');
 r.db("CollabDB").table('points').insert([{project: "Stadt", x: 10, y:20, z:30}]);
 r.db("CollabDB").table('points').insert([{project: "Stadt", x: 20, y:30, z:30}]);
 r.db("CollabDB").table('points').insert([{project: "Haus", x: 20, y:30, z:30}]);
-
+r.db("CollabDB").table('points').insert([{project: "Stadt", x: 90, y:20, z:40}]);
+r.db("CollabDB").table('points').insert([{project: "Stadt", x: 80, y:10, z:20}]);
+r.db("CollabDB").table('points').insert([{project: "Haus", x: 70, y:10, z:20}]);

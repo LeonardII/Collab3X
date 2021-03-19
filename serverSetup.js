00431c893b29ab5056938cdf02c02b839a585f47
Leonard <leonard.faix@googlemail.com>
2021-03-19 17:43:42 +0100

    removed

--- serverSetup.js
+++
@@ -1,7 +0,0 @@
-
-r.dbCreate("CollabDB");
-r.db("CollabDB").tableCreate('points');
-r.db("CollabDB").table('points').insert([{project: "Stadt", x: 10, y:20, z:30}]);
-r.db("CollabDB").table('points').insert([{project: "Stadt", x: 20, y:30, z:30}]);
-r.db("CollabDB").table('points').insert([{project: "Haus", x: 20, y:30, z:30}]);
-
